efdb26bfee5b6d88adfc1fd00af2ef9cc80705d0
Chrysanthus Jumaa <jumaachrysanthus@gmail.com>
2025-07-31 12:06:52 +0300

    Created variables to store input

--- calculator.py
+++ calculator.py
@@ -1,1 +1,4 @@
 # CREATING A SIMPLE CALCULATOR PROGRAM
+# CREATING A VARIABLES THAT STORE THE USER'S INPUT AFTER PROMPT
+first_number = float(input("Please enter the first number: ")) #ACCEPTING FIRST NUMBER
+second_number = float(input("Please enter the second number: ")) #ACCEPTING SECOND NUMBER
